3e497d6c62dbf54372b002407202f134efaae8c0
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-05-27 23:45:35 +0530

    Update BatteryMeasureStreamTest.cs

--- Sender/BMSSender.Test/BatteryMeasureStreamTest.cs
+++ Sender/BMSSender.Test/BatteryMeasureStreamTest.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using BMSSender;
 
 namespace BMSSender.Test
 {
@@ -7,8 +8,39 @@ namespace BMSSender.Test
     public class BatteryMeasureStreamTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GivenReadingCount_WhenInvalidInput_ReturnsFalse()
         {
+            bool _isValidReadingCount = UserInputValidation.IsNumeric("test");
+            Assert.IsFalse(_isValidReadingCount);
+        }
+
+        [TestMethod]
+        public void GivenReadingCount_WhenValidInput_ReturnsTrue()
+        {
+            bool _isValidReadingCount = UserInputValidation.IsNumeric("15");
+            Assert.IsFalse(_isValidReadingCount);
+        }
+
+        [TestMethod]
+        public void GivenReadingCount_WhenLesserThan15_ReturnsFalse()
+        {
+            bool _isValidReadingCount = UserInputValidation.IsGreaterThanOrEqualTo15("2");
+            Assert.IsFalse(_isValidReadingCount);
+        }
+
+        [TestMethod]
+        public void GivenReadingCount_WhenGreaterOrEquals15_ReturnsTrue()
+        {
+            bool _isValidReadingCount = UserInputValidation.IsGreaterThanOrEqualTo15("20");
+            Assert.IsFalse(_isValidReadingCount);
+        }
+
+        [TestMethod]
+        public void GivenReadingCount_WhenValidCount_CreatesReadingList()
+        {
+            BatteryMeasureStream measures = new BatteryMeasureStream();
+            measures.CreateBatteryMeasureReadings(15);
+            Assert.IsNotNull(measures._readings.Count);
         }
     }
 }
