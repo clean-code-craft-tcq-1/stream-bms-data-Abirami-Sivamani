0e604bf34d4d07b16af7bd95507f78c4667cf06c
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-05-27 22:17:16 +0530

    Create BatteryMeasure.cs

---
+++ Sender/BMSSender/BatteryMeasure.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMSSender
+{
+    public class BatteryMeasures
+    {
+        double Temperature { get; set; }
+        double soc { get; set; }
+    }
+}
