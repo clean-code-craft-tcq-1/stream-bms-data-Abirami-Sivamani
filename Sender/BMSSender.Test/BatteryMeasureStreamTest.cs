c27aa0ddf7efcfa9123434f830c4ce5c3d2b5d44
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-05-27 22:20:31 +0530

    Create BatteryMeasureStreamTest.cs

---
+++ Sender/BMSSender.Test/BatteryMeasureStreamTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BMSSender.Test
+{
+    [TestClass]
+    public class BatteryMeasureStreamTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
