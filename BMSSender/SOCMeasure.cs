a06137abc1134e4e4efa7f85bec5ba516cede1b4
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-06-08 18:02:44 +0530

    Create SOCMeasure.cs

---
+++ BMSSender/SOCMeasure.cs
@@ -0,0 +1,18 @@
+using BMSSender.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMSSender
+{
+    public class SOCMeasure : IBMSGenerator
+    {
+        Random _randomNumber = new Random();
+        public double BatteryMeasureValueGenerator()
+        {
+            return Math.Round((_randomNumber.NextDouble() * (BatteryExtremeLimit.MaxSoc - BatteryExtremeLimit.MinSoc) + BatteryExtremeLimit.MinTemperature), 2);
+        }
+    }
+}
